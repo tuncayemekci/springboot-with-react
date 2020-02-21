41db1a9c54a997157ea3d6c10271cfb60323c2b6
Tuncay Alp Emekci <tuncayemekci@gmail.com>
2020-02-21 20:20:14 +0300

    Initial commit
    Project is created by Spring Initialzr

---
+++ src/test/java/com/tuncay/demo/SpringBootwithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuncay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootwithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
